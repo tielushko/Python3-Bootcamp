e7ab7403e5b2353581f4f7e2f81f6a4e331e457f
Oleg Tielushko <tielushko@mail.usf.edu>
2020-10-10 17:43:44 -0400

    Update bs_basics.py

--- Section 32 - Scraping with Beautiful Soup/bs_basics.py
+++ Section 32 - Scraping with Beautiful Soup/bs_basics.py
@@ -1,4 +1,4 @@
-import bs4
+from bs4 import BeautifulSoup
 
 html = """
 <!DOCTYPE html>
@@ -21,3 +21,43 @@ html = """
 </body>
 </html>
 """
+
+soup = BeautifulSoup(html, "html.parser")
+
+#HTML SELECTORS
+
+#selecting all using html tags - find all returns a list with tags.
+print(soup.find_all("li"))
+print()
+
+#selecting the element by id
+print(soup.find(id="first"))
+print()
+
+#selecting the elements by class special. notice since class is reserved Python word we need to use the class_
+print(soup.find_all(class_="special"))
+print()
+
+#selecting elements which have their data attributes set to a specific value
+print(soup.find_all(attrs={"data-example" : "yes"}))
+print()
+
+#CSS SELECTORS
+
+#select - returns the list of the id even if only one element
+print(soup.select("#first"))
+print()
+
+# by class
+print(soup.select(".special"))
+print()
+
+# by tag 
+print(soup.select("div"))
+print()
+
+# by attribute
+print(soup.select("[data-example]"))
+print()
+
+
